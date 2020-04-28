b98a615b44d8b6bd843c85bd74ed55b0ce6495ae
brayo333 <mulaabrian@gmail.com>
2020-04-28 22:31:09 +0300

    new

--- lab1.php
+++ lab1.php
@@ -7,14 +7,27 @@
         $first_name = $_POST['first_name'];
         $last_name = $_POST['last_name'];
         $city = $_POST['city_name'];
+        $uname = $_POST['username'];
+        $pass = $_POST['password'];
 
-        $user = new User($first_name, $last_name, $city);
-        $res = $user->save();
+        $user = new User($first_name, $last_name, $city, $uname, $pass);
 
-        if($res){
-            echo "Save operation was successful";
+        if(!$user->validateForm()){
+            $user->createFormErrorSessions();
+            header("Refresh:0");
+            die();
+        }
+
+        if(!$user->isUserExist()){
+            $res = $user->save();
+            
+            if($res){
+                echo "Save operation was successful";
+            }else{
+                echo "An error occured!";
+            }
         }else{
-            echo "An error occured!";
+            echo "Username already exists";
         }
     }
 
@@ -22,10 +35,12 @@
         $first_name=0;
         $last_name=0;
         $city=0;
+        $uname=0;
+        $pass=0;
 
-        $user = new User($first_name, $last_name, $city);
+        $user = new User($first_name, $last_name, $city, $uname, $pass);
         $res = $user->readAll();
-        
+
         echo "<!DOCTYPE html>
                     <html>
                         <head>
@@ -40,6 +55,8 @@
                                     <th>First Name</th>
                                     <th>Last Name</th>
                                     <th>City</th>
+                                    <th>Username</th>
+                                    <th>Password</th>
                                 </tr>
 
                                 <tr>";		
@@ -49,12 +66,16 @@
                                             $first_name = $row['first_name'];
                                             $last_name = $row['last_name'];
                                             $city = $row['user_city'];
+                                            $uname = $row['username'];
+                                            $pass = $row['user_pass'];
 
                                         echo "<tr>
                                         <td>".$user_id."</td>
                                         <td>".$first_name."</td>
                                         <td>".$last_name."</td>
                                         <td>".$city."</td>
+                                        <td>".$uname."</td>
+                                        <td>".$pass."</td>
                                         </tr>";
                                         }
                                         
@@ -75,11 +96,27 @@
 <html>
 	<head>
 		<title>Lab 1</title>
+        <script type="text/javascript" src="validate.js"></script>
+        <link rel="stylesheet" type="text/css" href="validate.css">
 	</head>
 	
 	<body>
-		<form method="post">
-			<table>
+		<form method="post" name="user_details" id="user_details" onsubmit="return validateForm()" action="<?=$_SERVER['PHP_SELF']?>">
+			<table align="center">
+                <tr>
+                    <td>
+                        <div id="form-errors">
+                            <?php
+                                session_start();
+                                if(!empty($_SESSION['form_errors'])){
+                                    echo " " . $_SESSION['form_errors'];
+                                    unset($_SESSION['form_errors']);
+                                }
+                            ?>
+                        </div>
+                    </td>
+                </tr>
+
                 <tr>
                     <td><input type='text' name='first_name' required placeholder='First Name'/></td>
                 </tr>
@@ -90,7 +127,16 @@
                     <td><input type='text' name='city_name' placeholder='City'/></td>
                 </tr>
                 <tr>
+                    <td><input type='text' name='username' placeholder='Username'/></td>
+                </tr>
+                <tr>
+                    <td><input type='password' name='password' placeholder='Password'/></td>
+                </tr>
+                <tr>
                     <td><button type='submit' name='btn-save'>SAVE</button></td>
+                </tr>
+                <tr>
+                    <td><a href='login.php'>Login</a></td>
                 </tr>
                 <tr>
                     <td><a href='lab1.php?hello=true'>Run readAll function</a></td>
--- user.php
+++ user.php
@@ -1,5 +1,6 @@
 <?php
     include "crud.php";
+    include "authenticator.php";
     include_once "DBConnector.php";
 
     class User implements Crud{
@@ -7,11 +8,36 @@
         private $first_name;
         private $last_name;
         private $city_name;
+        private $username;
+        private $password;
 
-        function __construct($first_name, $last_name, $city_name){
+        function __construct($first_name, $last_name, $city_name, $username, $password){
             $this->first_name = $first_name;
             $this->last_name = $last_name;
             $this->city_name = $city_name;
+            $this->username = $username;
+            $this->password = $password;
+        }
+
+        public static function create(){
+            $instance = new self($first_name, $last_name, $city_name, $username, $password);
+            return $instance;
+        }
+
+        public function setUsername($username){
+            $this->username = $username;
+        }
+
+        public function getUsername(){
+            return $this->username;
+        }
+
+        public function setPassword($password){
+            $this->password = $password;
+        }
+
+        public function getPassword(){
+            return $this->password;
         }
 
         public function setUserId ($user_id){
@@ -22,14 +48,71 @@
             return $this->user_id;
         }
 
+        public function isUserExist(){
+            $username = $this->username;
+            $con = new DBConnector;
+            $found = false;
+            $res = mysqli_query($con->conn, "SELECT * FROM user") or die("Error: " .mysqli_error());
+
+            while($row = mysqli_fetch_array($res)){
+                if($row['username'] == $username){
+                    $found = true;
+                }
+            }
+
+            $con->closeDatabase();
+            return $found;
+        }
+
+        public function hashPassword(){
+            $this->password = password_hash($this->password, PASSWORD_DEFAULT);
+        }
+
+        public function isPasswordCorrect (){
+            $con = new DBConnector;
+            $found = false;
+            $res = mysqli_query($con->conn, "SELECT * FROM user") or die("Error: " .mysqli_error());
+
+            while($row = mysqli_fetch_array($res)){
+                if(password_verify($this->getPassword(), $row['user_pass']) && $this->getUsername() == $row['username']){
+                    $found = true;
+                }
+            }
+
+            $con->closeDatabase();
+            return $found;
+        }
+
+        public function login(){
+            if($this->isPasswordCorrect()){
+                header("Location:private_page.php");
+            }
+        }
+
+        public function createUserSession(){
+            session_start();
+            $_SESSION['username'] = $this->getUsername();
+        }
+
+        public function logout(){
+            session_start();
+            unset($_SESSION['username']);
+            session_destroy();
+            header("Location:lab1.php");
+        }
+
         public function save(){
             $fn = $this->first_name;
             $ln = $this->last_name;
             $city = $this->city_name;
+            $uname = $this->username;
+            $this->hashpassword();
+            $pass = $this->password;
 
             $con = new DBConnector;
 
-            $res = mysqli_query($con->conn, "INSERT INTO user(first_name, last_name, user_city) VALUES('$fn', '$ln', '$city')") or die("Error: " .mysql_error());
+            $res = mysqli_query($con->conn, "INSERT INTO user(first_name, last_name, user_city, username, user_pass) 
+                VALUES('$fn', '$ln', '$city', '$uname', '$pass')") or die("Error: " .mysql_error());
             return $res;
         }
 
@@ -58,6 +141,22 @@
 
         public function removeAll(){
             return null;
+        }
+
+        public function validateForm(){
+            $fn = $this->first_name;
+            $ln = $this->last_name;
+            $city = $this->city_name;
+
+            if($fn =="" || $ln=="" || $city==""){
+                return false;
+            }
+            return true;
+        }
+
+        public function createFormErrorSessions(){
+            session_start();
+            $_SESSION['form_errors'] = "All fields are required";
         }
     }
 ?>
