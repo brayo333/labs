636e761e0a07b91e21a2d21a6c60f32f3c6b261a
brayo333 <mulaabrian@gmail.com>
2020-07-16 22:12:39 +0300

    Updated lab

--- private_page.php
+++ private_page.php
@@ -3,67 +3,29 @@
     if(!isset($_SESSION['username'])){
         header("Location:login.php");
     }
-
-    include_once 'DBConnector.php';
-    if($_SERVER['REQUEST_METHOD'] !== 'POST'){
-        header('HTTP/1.0 403 Forbidden');
-        echo 'You are forbidden';
-    }else{
-        $api_key = null;
-        $api_key = generateApiKey(64);
-        header('Content-type: application/json');
-
-        echo generateResponse($api_key);
-    }
-
-    function generateApiKey($str_length){
-        $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $bytes = openssl_random_pseudo_bytes(3*$str_length/4+1);
-
-        $repl = unpack('C2', $bytes);
-
-        $first = $chars[$repl[1]%62];
-        $second = $chars[$repl[2]%62];
-        return strtr(substr(base64_encode($bytes), 0, $str_length), '+/', "$first$second");
-    }
-
-    function saveApiKey(){
-        
-        return true;
-    }
-
-    function generateResponse($api_key){
-        if(saveApiKey()){
-            $res = ['success' => 1, 'message' => $api_key];
-        }else{
-            $res = ['success' => 0, 'message' => 'Something went wrong. Please regenerate the API key'];
-        }
-        return json_encode($res);
-    }
-
-    function fetchUserApiKey(){}
-
 ?>
 
 <html>
     <head>
         <title>PrivatePage</title>
-        <script src="jquery-3.1.1.min.js"></script>
-        <script type ="text/javascript" src="validate.js"></script>
+        <script src="jquery-3.5.1.min.js"></script>
         <script type ="text/javascript" src="apikey.js"></script>
+        <script type ="text/javascript" src="validate.js"></script>
         <link rel="stylesheet" type="text/css" href="validate.css">
 
-        <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
-        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script> -->
 
-        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootsrap.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+        <!-- <link rel="stylesheet" type="text/css" href="bootstrap/css/bootsrap.css">
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootsrap.css.map">
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootsrap.min.css">
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootsrap.min.css.map">
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootsrap-theme.css">
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootsrap-theme.css.map">
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootsrap-theme.min.css">
-        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootsrap-theme.min.css.map">
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootsrap-theme.min.css.map"> -->
     </head>
     <body>
         <p>This is a private page</p>
@@ -78,7 +40,7 @@
 
         <strong>Your API Key:</strong>(Note that if your API Key is already in use by already running applications, 
         generating a new key will stop the application from functioning)<br>
-        <textarea cols="100" row="2" id="api_key" name="api_key" readonly><?php echo fetchUserApiKey();?></textarea>
+        <textarea cols="100" row="2" id="api_key" name="api_key" readonly><?php include "apikey.php"; echo fetchUserApiKey();?></textarea>
 
         <h3>Service description:</h3>
         We have a service/API that allows external applications to order food and also pull all 
